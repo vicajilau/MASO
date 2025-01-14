ad4320e5c884daf041336b245b8672475b3b540c
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-01-14 18:25:45 +0100

    Removed unused factory method

--- lib/domain/models/maso/i_process.dart
+++ lib/domain/models/maso/i_process.dart
@@ -1,7 +1,3 @@
-import 'package:maso/domain/models/maso/regular_process.dart';
-
-import 'burst_process.dart';
-
 /// The `IProcess` abstract class represents a generic process with common attributes.
 /// It serves as the base class for different types of processes, such as `RegularProcess`
 /// and `BurstProcess`.
@@ -29,18 +25,6 @@ abstract class IProcess {
     required this.ioDevice,
     this.executionTime = 0,
   });
-
-  /// Factory constructor to create an `IProcess` instance from a JSON map.
-  /// Depending on the mode, it returns either a `RegularProcess` or a `BurstProcess`.
-  factory IProcess.fromJson(Map<String, dynamic> json, String mode) {
-    if (mode == "regular") {
-      return RegularProcess.fromJson(json);
-    } else if (mode == "burst") {
-      return BurstProcess.fromJson(json);
-    } else {
-      throw ArgumentError("Invalid mode: $mode");
-    }
-  }
 
   /// Converts the `IProcess` instance to a JSON map.
   Map<String, dynamic> toJson();
