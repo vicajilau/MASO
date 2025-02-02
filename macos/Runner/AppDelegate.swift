6590c6476b0111667619ef971163fee49f7d0a92
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-02-02 18:21:27 +0100

    MacOS working

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,11 +3,70 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        let appPath = Bundle.main.bundlePath
+        NSLog("La ubicación de la aplicación es: \(appPath)")
+    }
+    
+    override func application(_ application: NSApplication, open urls: [URL]) {
+        NSLog("🔍 macOS intentó abrir un archivo con open")
+        for url in urls {
+            NSLog("📂 Archivo recibido en open urls: \(url.path)")
+            sendToFlutter(url.path)
+        }
+    }
+    
+    override func application(_ sender: NSApplication, openFile filename: String) -> Bool {
+        NSLog("✅ Se llamó a openFile con: \(filename)")
+        
+        sendToFlutter(filename)
+        
+        return true
+    }
+    
+    override func application(_ sender: NSApplication, openFiles filenames: [String]) {
+        NSLog("🔍 macOS intentó abrir un archivo con openFiles")
+        for url in filenames {
+            sendToFlutter(url)
+        }
+    }
+    
+    override func application(_ sender: Any, openFileWithoutUI filename: String) -> Bool {
+        NSLog("🔍 macOS intentó abrir un archivo con openFileWithoutUI")
+        NSLog("📂 Archivo recibido en open urls: \(filename)")
+        sendToFlutter(filename)
+        return true
+    }
+    
+    override func application(_ sender: NSApplication, openTempFile filename: String) -> Bool {
+        NSLog("🔍 macOS intentó abrir un archivo con openTempFile")
+        NSLog("📂 Archivo recibido en open urls: \(filename)")
+        sendToFlutter(filename)
+        return true
+    }
+    
+    override func applicationOpenUntitledFile(_ sender: NSApplication) -> Bool {
+        NSLog("🔍 macOS intentó abrir un archivo con applicationOpenUntitledFile")
+        return true
+    }
+    
+    override func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
+        NSLog("🔍 macOS intentó abrir un archivo con applicationShouldOpenUntitledFile")
+        return true
+    }
+    
+    func sendToFlutter(_ filename: String) {
+        if let controller = mainFlutterWindow?.contentViewController as? FlutterViewController {
+            let channel = FlutterMethodChannel(name: "maso.file", binaryMessenger: controller.engine.binaryMessenger)
+            channel.invokeMethod("openFile", arguments: filename)
+            NSLog("📢 Se envió el archivo a Flutter: \(filename)")
+        } else {
+            NSLog("❌ No se encontró el controlador de Flutter")
+        }
+    }
+    
 }
